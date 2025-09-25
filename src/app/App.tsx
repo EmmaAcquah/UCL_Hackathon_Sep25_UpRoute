7c20027e56551c48e4fffc64bd276468e68f3e77
Alex Hort-Francis <ajhf@hotmail.co.uk>
2025-09-25 12:11:52 +0100

    feat: Add FloorPlan SVG code - Make FloorPlan component - Add FloorPlan to App - Update package name

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react';
 import { useRealtimeAgent } from './hooks/';
 import { getWeather, createDisplayTextTool } from './tools';
+import FloorPlan from './components/FloorPlan';
 import './App.css';
 
 function App() {
@@ -16,9 +17,10 @@ function App() {
   return (
     <>
       <main>
-        <h1>OpenAI Realtime Agent Deployment Spike 01</h1>
+        <FloorPlan />
+        {/* <h1>OpenAI Realtime Agent Deployment Spike 01</h1>
         <div>Output:</div>
-        <div>{displayText}</div>
+        <div>{displayText}</div> */}
       </main>
     </>
   );
